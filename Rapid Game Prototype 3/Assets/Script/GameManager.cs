33229317ac5edfd1ae4cf6d7849cda523982620a
NormyHaddad <normy.haddad@gmail.com>
2021-07-16 14:21:12 +1000

    Added customer and state machine

--- Rapid Game Prototype 3/Assets/Script/GameManager.cs
+++ Rapid Game Prototype 3/Assets/Script/GameManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject customerPrefab;
+
+    //for testing, can remove
+    float timer = 10;
+
+    GameObject customer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        customer = Instantiate(customerPrefab, customerPrefab.GetComponent<CustomerController>().spawnPos.position, Quaternion.identity);   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
